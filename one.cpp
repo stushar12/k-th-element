03162de9720c085d0bf265ecd6c4f27d907337cd
Tushar Saxena <stushar345@gmail.com>
2021-06-03 02:33:57 +0530

    Add files via upload

---
+++ one.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        if(n > m) 
+        {
+            return kthElement(arr2, arr1, m, n, k); 
+        }
+        
+        int low = max(0,k-m);    //if k=7 and m=6 then u have to take atleast 1 elemnt from arr1 low can't be 0
+        int  high = min(k,n);    //if k=3 and n=4 then high should be 3
+        
+        while(low <= high) {
+            int cut1 = (low + high) >> 1; 
+            int cut2 = k - cut1; 
+            int l1 = cut1 == 0 ? INT_MIN : arr1[cut1 - 1]; 
+            int l2 = cut2 == 0 ? INT_MIN : arr2[cut2 - 1];
+            int r1 = cut1 == n ? INT_MAX : arr1[cut1]; 
+            int r2 = cut2 == m ? INT_MAX : arr2[cut2]; 
+            
+            if(l1 <= r2 && l2 <= r1) {
+                return max(l1, l2);
+            }
+            else if (l1 > r2) {
+                high = cut1 - 1;
+            }
+            else {
+                low = cut1 + 1; 
+            }
+        }
+        return 1; 
+    }
